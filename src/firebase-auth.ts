8206ef336bb0f5ed3f3124d48382cbbe84d22694
mijor <mijor@example.com>
2026-06-10 16:24:52 +0200

    fix: dodavanje select_account na Google provider

--- src/firebase-auth.ts
+++ src/firebase-auth.ts
@@ -3,3 +3,6 @@ import { app } from './firebase-app';
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
